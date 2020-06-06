4812ebbc3ed9c854106b83c3d3825403284346a6
aguma <agumabanksibrahim@gmail.com>
2020-06-06 08:43:21 +0300

    first commit

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "sanaa";
+// MySQL Database Password
+$sql_db_pass = "sanaa";
+// MySQL Database Name
+$sql_db_name = "sanaa";
+
+// Site URL
+$site_url = "http://localhost:8888/script"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "ss"; // Your purchase code, don't give it to anyone. 
+?>
